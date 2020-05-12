357c3cf2354501f6ae971d6590b8353f4040d755
GhubOsua <ofugeno@ya.ru>
2020-05-12 17:00:42 +0500

    new

--- script_raid.sh
+++ script_raid.sh
@@ -4,5 +4,6 @@ sudo yum -y install mdadm
 sudo mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
 sudo mdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{b,c,d,e,f}
 cat /proc/mdstat
-#sudo bash -c "echo "DEVICE partitions" > /etc/mdadm/mdadm.conf"
-sudo bash -c 'echo "DEVICE partitions" > /etc/mdadm/mdadm.conf && mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf'
+sudo echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+sudo mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf'
+#sudo bash -c 'echo "DEVICE partitions" > /etc/mdadm/mdadm.conf && mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf'
