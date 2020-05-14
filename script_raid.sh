1d12c1dccaa486c261848a36c2b7f29e162ad509
Alex <64502957+GhubOsua@users.noreply.github.com>
2020-05-14 12:49:46 +0500

    Update script_raid.sh

--- script_raid.sh
+++ script_raid.sh
@@ -5,8 +5,8 @@ sudo yum -y install mdadm
 sudo mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
 sudo mdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{b,c,d,e,f}
 cat /proc/mdstat
-sudo echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
-sudo mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf'
+sudo bash -c "echo \"DEVICE partitions\" > /etc/mdadm/mdadm.conf"
+sudo bash -c "mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf"
 #sudo bash -c 'echo "DEVICE partitions" > /etc/mdadm/mdadm.conf && mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf'
 echo '============================='
 echo '1. Создание GPT раздедела на RAID'
